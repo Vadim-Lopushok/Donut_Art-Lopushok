6b295e2c0f852bf31247b80477560e767d29b23d
mighty <mighty2815736@gmail.com>
2022-01-07 22:44:21 +0300

    Revert "renamed files and fix structure"
    This reverts commit dbd715e07e57eda601621a70d82510b89f0e4e74.

--- project 0.1/script/main.js
+++ project 0.1/script/main.js
@@ -1,2 +1,16 @@
-'use strict'
+window.onscroll = function () {
+  myFunction();
+};
 
+var navbar = document.getElementById("navbar");
+var cat = document.getElementById("text");
+
+var sticky = text.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("fixed--top");
+  } else {
+    navbar.classList.remove("fixed--top");
+  }
+}
