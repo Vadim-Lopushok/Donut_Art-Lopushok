dbd715e07e57eda601621a70d82510b89f0e4e74
mighty <mighty2815736@gmail.com>
2022-01-07 22:41:35 +0300

    renamed files and fix structure

--- project 0.1/script/main.js
+++ project 0.1/script/main.js
@@ -1,16 +1,2 @@
-window.onscroll = function () {
-  myFunction();
-};
+'use strict'
 
-var navbar = document.getElementById("navbar");
-var cat = document.getElementById("text");
-
-var sticky = text.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("fixed--top");
-  } else {
-    navbar.classList.remove("fixed--top");
-  }
-}
