16551f3c932b87b037cfa1100ebb83d130899d11
mighty <mighty2815736@gmail.com>
2022-01-09 22:58:28 +0300

    little issue

--- project 0.1/script/menu.js
+++ project 0.1/script/menu.js
@@ -5,9 +5,7 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 
 function checkBoxes() {
-    // const triggerBottom = window.innerHeight * 0.8;
-    // 0.8 is equivalent of 4/5
-    // So effectively we are saying that  triggerBottom is 80% of window.innerHeight.
+
     const triggerBottom = window.innerHeight / 5 * 3
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
