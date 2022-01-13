a90eed77d2b05ca5bb921fdb2142c61466ebe6fc
mighty <mighty2815736@gmail.com>
2022-01-13 18:22:01 +0300

    Page Our Team Components

---
+++ project 0.1/script/ourTeam.js
@@ -0,0 +1,12 @@
+'use strict'
+
+$(window).scroll(function() {
+  $('#portrait').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 400) {
+      $(this).addClass("slideUp");
+    }
+  });
+});
