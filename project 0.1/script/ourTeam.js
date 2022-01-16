845b1fe57e8a4eaaf360f85b05cd984c0d5b9e63
mighty <mighty2815736@gmail.com>
2022-01-16 20:05:47 +0300

    add current link menu func

--- project 0.1/script/ourTeam.js
+++ project 0.1/script/ourTeam.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-$(window).scroll(function() {
-  $('#portrait').each(function () {
+$(window).scroll(function () {
+  $("#portrait").each(function () {
     var imagePos = $(this).offset().top;
 
     var topOfWindow = $(window).scrollTop();
@@ -11,8 +11,8 @@ $(window).scroll(function() {
   });
 });
 
-$(window).scroll(function() {
-  $('#portrait2').each(function () {
+$(window).scroll(function () {
+  $("#portrait2").each(function () {
     var imagePos = $(this).offset().top;
 
     var topOfWindow = $(window).scrollTop();
@@ -22,8 +22,8 @@ $(window).scroll(function() {
   });
 });
 
-$(window).scroll(function() {
-  $('#portrait3').each(function () {
+$(window).scroll(function () {
+  $("#portrait3").each(function () {
     var imagePos = $(this).offset().top;
 
     var topOfWindow = $(window).scrollTop();
@@ -32,3 +32,54 @@ $(window).scroll(function() {
     }
   });
 });
+
+const imgs = document.getElementById("imgs");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+const img = document.querySelectorAll("#imgs img");
+
+let idx = 0;
+
+let interval = setInterval(run, 3000);
+
+function run() {
+  idx++;
+  changeImage();
+}
+
+function changeImage() {
+  if (idx > img.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+
+  imgs.style.transform = `translateX(${-idx * 640}px)`;
+}
+
+function resetInterval() {
+  clearInterval(interval)
+  interval = setInterval (run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+  idx++
+  changeImage()
+  resetInterval()
+})
+
+leftBtn.addEventListener('click', () => {
+  idx--
+  changeImage()
+  resetInterval()
+})
+
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('.menu__link');
+const menuLenght = menuItem.length
+for (let i = 0; i<menuLenght; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "active menu__link";
+  }
+}
