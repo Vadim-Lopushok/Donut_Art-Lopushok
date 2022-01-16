6ea06f4978c095149a00c170977f9c3ab2380b3d
mighty <mighty2815736@gmail.com>
2022-01-16 19:59:46 +0300

    add menu current link menu func

--- project 0.1/script/main.js
+++ project 0.1/script/main.js
@@ -51,3 +51,12 @@ function wireUpTriggers() {
     });
   });
 }
+
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('.menu__link');
+const menuLenght = menuItem.length
+for (let i = 0; i<menuLenght; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "active menu__link";
+  }
+}
