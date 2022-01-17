dfcc90cc052955a9f5f4df4f5e6d30033ccc2a5c
mighty <mighty2815736@gmail.com>
2022-01-18 00:29:26 +0300

    intersection observer func

--- project 0.1/script/blog.js
+++ project 0.1/script/blog.js
@@ -1,8 +1,26 @@
 const currentLocation = location.href;
 const menuItem = document.querySelectorAll('.menu__link');
 const menuLenght = menuItem.length
-for (let i = 0; i<menuLenght; i++) {
+for (let i = 0; i < menuLenght; i++) {
   if (menuItem[i].href === currentLocation) {
     menuItem[i].className = "active menu__link";
   }
+}
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('elem-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.conception__content');
+
+for (let elm of elements) {
+  observer.observe(elm);
 }
